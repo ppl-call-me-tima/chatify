627db0afe63f13b7d7f321503e22564340c3ac8f
Amit Upadhyay <2648amitupadhyay@gmail.com>
2024-09-28 14:01:41 +0530

    implement registration route with db

--- app.py
+++ app.py
@@ -24,13 +24,11 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        # For making a session permanent - so that it exists even after a browser is closed
-        session.permanent = True
-        
+        # Retrieve form data
         username = request.form.get("username")
         password = request.form.get("password")
 
-        # Validation of the input fields
+        # Validate form data
         if not username:
             print("Please enter a username")
             return redirect(url_for("login"))
@@ -39,15 +37,16 @@ def login():
             print("Please enter a password")
             return redirect(url_for("login"))
         
-        # Gets user data from the database
+        # Get user data from the database
         rows = execute_retrieve("SELECT id, hash FROM user WHERE username = :username", {"username" : username})
                         
-        # Checks if the user exists from database OR password doesn't match [fused for security]
+        # Check if user doesn't exist from db OR password doesn't match [fused for security]
         if not rows or not check_password_hash(rows[0]["hash"], password):
             print("Enter correct username / password!")
             return redirect(url_for("login"))
         
-        # Logs the user in, using its ID instead of username
+        # Logs the user in, using its ID
+        session.permanent = True  # For making a session permanent - so that it exists even after the browser is closed
         session["user_id"] = rows[0]["id"]
         return redirect(url_for("index"))
     else:
@@ -66,7 +65,45 @@ def logout():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == "POST":
-        pass
+    if request.method == "POST":        
+        # Retrieve form data
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        
+        # Validate form data
+        if not username:
+            print("Please enter a username.")
+            return redirect(url_for("register"))
+        
+        if not password:
+            print("Please enter a password.")
+            return redirect(url_for("register"))
+        
+        if not confirmation:
+            print("Please re-enter password.")
+            return redirect(url_for("register"))
+        
+        # Check if username already exists
+        rows = execute_retrieve("SELECT id FROM user WHERE username = :username", {"username":username})
+        
+        if rows:
+            print("Username already taken!")
+            return redirect(url_for("register"))
+        
+        # Check password and confirmation not matching
+        if password != confirmation:
+            print("Passwords don't match!")
+            return redirect(url_for("register"))
+        
+        # Insert data into table
+        execute("INSERT INTO user (username, hash) VALUES (:username, :hash)", 
+                {"username":username, "hash":generate_password_hash(password)})
+
+        # Log-in the user
+        rows = execute_retrieve("SELECT id FROM user WHERE username = :username", {"username":username})
+        session.permanent = True
+        session["user_id"] = rows[0]["id"]
+        return redirect(url_for("index"))
     else:
         return render_template("register.html")
